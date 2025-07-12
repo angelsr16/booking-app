31df66577ce6202ad6f1c68523158fa0160d5d2e
Angel <sanchezangel.16@outlook.com>
2025-07-12 11:35:45 -0600

    added auth, auth tests and initial layout

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
+
+import bcrypt from "bcryptjs";
+import path from "path";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on localhost:3000");
+});
