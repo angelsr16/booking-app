e6f73751261e76821e0f102b460c152975303e6a
Angel <sanchezangel.16@outlook.com>
2025-07-13 22:26:48 -0600

    add hotel feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,9 +6,17 @@ import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import myHotelRoutes from "./routes/my-hotels";
 
 import bcrypt from "bcryptjs";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  // api_key: process.env.CLOUDINARY_API_KEY,
+  // api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -27,6 +35,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
