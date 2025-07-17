df6f068fa4c258aee61fed6245645849a5b0aab6
Angel <sanchezangel.16@outlook.com>
2025-07-17 16:16:46 -0600

    Add hotel feature

---
+++ frontend/src/pages/AddHotel.tsx
@@ -0,0 +1,26 @@
+import { useMutation } from "@tanstack/react-query";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../hooks/useAppContext";
+import * as apiClient from "../api-client";
+
+const AddHotel = () => {
+  const { showToast } = useAppContext();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiClient.addMyHotel,
+    onSuccess: () => {
+      showToast({ type: "SUCCESS", message: "Hotel saved" });
+    },
+    onError: () => {
+      showToast({ type: "ERROR", message: "Error saving hotel" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return <ManageHotelForm onSave={handleSave} isLoading={isPending} />;
+};
+
+export default AddHotel;
