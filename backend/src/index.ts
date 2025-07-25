fe3d718f7412aee23f098b2dbdecfc0f11c674b4
Angel <sanchezangel.16@outlook.com>
2025-07-25 15:12:14 -0600

    deploy new features

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -37,6 +37,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
 
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
