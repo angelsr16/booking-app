b15ae458ef71167b0edafb9212cd0fdae267e81c
Angel <sanchezangel.16@outlook.com>
2025-07-25 15:23:39 -0600

    fix * path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -37,7 +37,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
 
-app.get("*", (req: Request, res: Response) => {
+app.get("/*", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
